f9f00788437150bd81d42f8235f7159790b4d2c3
Dishant Jain <dishantkcity@gmail.com>
2020-01-13 12:28:20 +0530

    Heroes Completed

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -29,5 +29,13 @@ export class HeroDetailComponent implements OnInit{
     goBack(): void {
         this.location.back();
     }
-    
+
+    save(): void {
+        this.heroService.updateHero(this.hero)
+          .subscribe(() => this.goBack());
+      }
+    delete(): void{
+        this.heroService.deleteHero(this.hero)
+          .subscribe(() => this.goBack());
+    }
 }
